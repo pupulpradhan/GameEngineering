ac82ea97af6dc954d41876bbbb3f78a12403e401
pupulpradhan <u1319905@utah.edu>
2022-09-03 19:04:20 -0600

    Update MonsterChase.cpp

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -6,13 +6,6 @@
 #include <string.h>
 using namespace std;
 
-struct Monster
-{
-    char name[20];
-    int xCoord;
-    int yCoord;
-};
-
 int main(int argc, char* argv[])
 {
     int monsterCount;
@@ -24,29 +17,30 @@ int main(int argc, char* argv[])
     int yDir = 0;
     int xDirPlayer = 0;
     int yDirPlayer = 0;
+    char** new_monsterName;
     cout << "Welcome to Monsters Chase!\n";
     cout << "How many Monsters do you want to play with?\n";
     cin >> monsterCount;
-    char** monsterName = new char* [monsterCount];
-    /*char** monsterName1 = malloc(monsterCount * sizeof(char));*/
-    /*for (int i = 0; i < monsterCount; i++)
-        monsterName[i] = new char[10];*/
+    char** monsterName = (char**)malloc(monsterCount * sizeof(char*));
     cout << "\nEnter the name for Player:\t";
     cin >> playerName;
     for (int i = 0; i < monsterCount; i++) {
         cout << "\nEnter the name for Monster " << i << ":\t";
         cin >> input;
-        monsterName[i] = new char[strlen(input) + 1];
-        memcpy(monsterName[i], input, strlen(input) + 1);
+        monsterName[i] = (char*)malloc((strlen(input) + 1) * sizeof(char));
+        memcpy(monsterName[i], input, strlen(input)+1);
     }
+    /*for (int i = 0; i < monsterCount; i++) {
+        cout << monsterName[i];
+    }*/
     int value;
     do {
-        if (monsterCount >= 5)
+        if (xDir >= 50)
             monsterCount--;
         cout << "Monsters: \n";
         for (int i = 0; i < monsterCount; i++) {
-            xDir = xDir + 2;
-            yDir = yDir + 3;
+            xDir = xDir == 60 ? 0 : xDir + 2;
+            yDir = yDir == 60 ? 0 : yDir + 3;
             cout << monsterName[i] << " is at " << "[ " << xDir << ", " << yDir << "]\n";
         }
         cout << "Player: \n";
@@ -69,23 +63,18 @@ int main(int argc, char* argv[])
         }
         if (value == 109) { //m add monster
             //add monster
-            /*monsterCount++;
-            char** new_monsterName = new char* [monsterCount];
-            memcpy(new_monsterName, monsterName, monsterCount);
-            delete[] monsterName;
-            char** monsterName = new char* [monsterCount];
-            memcpy(monsterName, new_monsterName, monsterCount);
-            delete[] new_monsterName;
-            monsterName[monsterCount - 1] = nameArray[nameArrayPointer];
-            nameArrayPointer = nameArrayPointer == 9 ? 0 : nameArrayPointer++;*/
+            monsterCount++;
+            new_monsterName = (char**)realloc(monsterName, sizeof(char*) * monsterCount);
+            if (new_monsterName == NULL) {
+                exit(1);
+            }
+            else {
+                monsterName = new_monsterName;
+                monsterName[monsterCount-1] = nameArray[nameArrayPointer];
+                nameArrayPointer = nameArrayPointer==9 ? 0 : nameArrayPointer+1;
+            }
         }
         if (value == 113) { //q quit
-            //Delete the array created
-            for (int i = 0; i < monsterCount; i++) {    //To delete the inner arrays
-                delete[] monsterName[i];
-            }
-            delete[] monsterName;
-
             return 0;
         }
     } while (value != 113);
