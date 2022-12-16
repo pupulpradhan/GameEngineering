332789127a792d12c39d36e860bac4d13cefedaf
pupulpradhan <u1319905@utah.edu>
2022-12-16 14:23:09 -0700

    Update MemorySystem.cpp

--- FixedSizeAllocator/FixedSizeAllocator/MemorySystem.cpp
+++ FixedSizeAllocator/FixedSizeAllocator/MemorySystem.cpp
@@ -1,7 +1,11 @@
 #include "MemorySystem.h"
+#include "HeapManagerProxy.h"
+
+using namespace HeapManagerProxy;
 
 bool InitializeMemorySystem(void * i_pHeapMemory, size_t i_sizeHeapMemory, unsigned int i_OptionalNumDescriptors)
 {
+	CreateHeapManager(i_pHeapMemory, i_sizeHeapMemory, i_OptionalNumDescriptors);
 	// create your HeapManager and FixedSizeAllocators
 	return true;
 }
@@ -10,6 +14,8 @@ void Collect()
 {
 	// coalesce free blocks
 	// you may or may not need to do this depending on how you've implemented your HeapManager
+	//HeapManager* i_pManager;
+	Collect();
 }
 
 void DestroyMemorySystem()
